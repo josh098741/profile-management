a4db26d01588db47ae72192cf3e6ce7f099b8034
josh098741 <morongemokaya@gmail.com>
2025-10-21 18:39:50 +0300

    Implementation of zustand

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,34 @@ import { Toaster } from 'react-hot-toast'
 import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import ProtectRoute from './components/ProtectRoute'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return(
-    <div>
-      <Toaster />
-      <Routes>
-        <Route path="/" element={< Login/>} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
+    <div className="flex flex-col">
+      <div className="">
+        <Header />
+        <Toaster />
+      </div>
+      <div className="flex flex-1">
+        <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/signup" element={<Signup />} />
+          <Route 
+            path="/home" 
+            element={
+              <ProtectRoute>
+                <Home />
+              </ProtectRoute>
+            } 
+          />
+        </Routes>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   )  
 }
